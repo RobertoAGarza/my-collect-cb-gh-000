06556349dccdb361e0ad2c00d53dea0742c6a0c5
Roberto Garza <robertg4567@hotmail.com>
2018-07-20 18:18:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   newCollection = []
   while i < array.length
-    newCollection << yield (array[i])
+    newCollection << yield(array[i])
     i += 1
   end
   newCollection
